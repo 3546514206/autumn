cec89071b18aac0cc5522c31a5964b72424a88e0
杨海波 <3546514206@qq.com>
2022-01-11 11:39:21 +0800

    FileSystemResource

--- src/main/java/edu/zjnu/autumn/core/io/UrlResource.java
+++ src/main/java/edu/zjnu/autumn/core/io/UrlResource.java
@@ -1,7 +1,12 @@
 package edu.zjnu.autumn.core.io;
 
+import sun.jvm.hotspot.ui.tree.FieldTreeNodeAdapter;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * @description: UrlResource
@@ -10,8 +15,25 @@ import java.io.InputStream;
  **/
 public class UrlResource implements Resource {
 
+    private final URL url;
+
+    public UrlResource(URL url) {
+        assert null != url;
+        this.url = url;
+    }
+
     @Override
     public InputStream getInputSteam() throws IOException {
-        return null;
+        URLConnection connection = this.url.openConnection();
+
+        try {
+            return connection.getInputStream();
+        } catch (IOException ex) {
+            if (connection instanceof HttpURLConnection) {
+                ((HttpURLConnection) connection).disconnect();
+            }
+            throw ex;
+        }
+
     }
 }
